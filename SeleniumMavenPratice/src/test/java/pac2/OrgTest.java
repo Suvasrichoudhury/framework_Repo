7ae7c5e4673946fc411204bf0f4b887038b93898
suvasrichoudhury <choudhurysuvasri13@gmail.com>
2024-08-16 16:28:51 +0530

    framework added

---
+++ SeleniumMavenPratice/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,16 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test(groups="smoke")
+	public void CreateOrgTest() {
+		System.out.println("executed CreateOrgTest");
+		
+	}
+	@Test(groups="regression")
+	public void modifyOrgTest() {
+		System.out.println("executed modifyOrgTest() ");
+		
+	}
+}
